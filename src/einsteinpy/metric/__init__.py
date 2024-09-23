da8409d1c1f25a44933aea28b2dccf5c2b3e2c8b
AlnesrAltair <yousef.tabatabai@gmail.com>
2024-09-23 21:48:29 +0100

    Updating metric __init__.py
    Updating __init__.py in the metric module to reference the Reissner-Nordstrom metric class

--- src/einsteinpy/metric/__init__.py
+++ src/einsteinpy/metric/__init__.py
@@ -2,5 +2,6 @@ from .base_metric import BaseMetric
 from .kerr import Kerr
 from .kerrnewman import KerrNewman
 from .schwarzschild import Schwarzschild
+from .reissnernordstrom import ReissnerNordstrom
 
-__all__ = ["BaseMetric", "Kerr", "KerrNewman", "Schwarzschild"]
+__all__ = ["BaseMetric", "Kerr", "KerrNewman", "Schwarzschild", "ReissnerNordstrom"]
