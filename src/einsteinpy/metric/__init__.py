0eadba0e89dfc76724adf370a6636e12b2f792bb
AlnesrAltair <yousef.tabatabai@gmail.com>
2024-09-24 10:30:22 +0100

    Adding tox reformating
    Reformatting changed files using tox. Note that some of the changes conflict with the PEP8 suggestions generated by the repository's checks.

--- src/einsteinpy/metric/__init__.py
+++ src/einsteinpy/metric/__init__.py
@@ -1,7 +1,7 @@
 from .base_metric import BaseMetric
 from .kerr import Kerr
 from .kerrnewman import KerrNewman
-from .schwarzschild import Schwarzschild
 from .reissnernordstrom import ReissnerNordstrom
+from .schwarzschild import Schwarzschild
 
 __all__ = ["BaseMetric", "Kerr", "KerrNewman", "Schwarzschild", "ReissnerNordstrom"]
